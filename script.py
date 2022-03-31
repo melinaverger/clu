02ba262426a2bcd3b82992da014bbec40c78b11c
Mélina Verger <melinaverger@Melinas-MacBook-Air-LIP6.local>
2022-03-31 16:27:18 +0200

    First commit

---
+++ script.py
@@ -0,0 +1,7 @@
+"""
+The goal of this script is to replicate a toy example for clustering analysis.
+"""
+
+import matplotlib.pyplot as plt
+#from sklearn.datasets import load_digits
+import sklearn
